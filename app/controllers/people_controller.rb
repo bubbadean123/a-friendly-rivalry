99efc49f654c4a62fd47af459c543a722190778f
Donovan Lampa <donovan.lampa@gmail.com>
2014-03-14 09:39:58 -0500

    Add person edit and removal

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,12 +1,41 @@
 class PeopleController < ApplicationController
 
   def new
+    @person = Person.new
   end
 
   def create
     @person = Person.new(params[:post])
-    @person.save
-    redirect_to @person
+
+    if @person.save
+      redirect_to @person
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update_attributes(
+        :combatant_name => params[:person][:combatant_name],
+        :combatant_description => params[:person][:combatant_description]
+        )
+      redirect_to @person
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+
+    redirect_to people_path
   end
 
   def show
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   attr_accessible :combatant_description, :combatant_name
+
+  validates :combatant_name, presence: true, length: { minimum: 1 }
 end
